a1c7f18b268f3ad8cd651cac382999c100e81fa6
AnserShafiq <ansershafiq@gmail.com>
2025-05-07 00:12:40 +0500

    May 06, 2025: An intro website for a payroll services providing canadian firm

--- src/Pages/PageSections/HomeSection6.jsx
+++ src/Pages/PageSections/HomeSection6.jsx
@@ -7,7 +7,7 @@ const HomeSection6 = () => {
 
   return (
     <div id='contact-section' className='flex flex-col lg:flex-row w-full mt-[2%] lg:mt-[3%]'>
-        <div className='w-full lg:w-[50%] lg:pl-4 lg:py-3 min-h-[50vh] lg:min-h-[75vh]'>
+        <div className='w-full lg:w-[50%] lg:pl-4 lg:py-3 lg:min-h-fit'>
             <img className='h-[50vh] lg:h-[100%] 2xl:h-[800px] rounded-[30px] w-[100%] object-cover' src={Sidebanner} alt='Contact Section Home'/>
         </div>
         <div className=' w-full lg:w-[50%] flex flex-col items-start justify-center px-[4%] py-[3%] '>
